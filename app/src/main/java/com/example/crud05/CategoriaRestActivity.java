8588db97465a31464d105a3971bd5b2dfe4916c7
Edwin Diaz <josephph1993@gmail.com>
2021-05-31 13:47:58 -0600

    Agregado el web service de las categorias

--- app/src/main/java/com/example/crud05/CategoriaRestActivity.java
+++ app/src/main/java/com/example/crud05/CategoriaRestActivity.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,8 +20,8 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 
 public class CategoriaRestActivity extends AppCompatActivity {
-    EditText etIdBuscar, etId, etNombreCategoria, etDescripcion;
-    Button btnIdBuscar, btnEliminar, btnTodosBuscar, btnAgregar, btnEditar;
+    EditText etNombreCategoria, etDescripcion;
+    Button btnTodosBuscar, btnAgregar;
 
     RecyclerView rvCategoria;
     List<Categoria> listaCategorias = new ArrayList<>();
@@ -34,41 +35,15 @@ public class CategoriaRestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categoria_rest);
-        etIdBuscar = findViewById(R.id.etIdBuscar);
-        etId = findViewById(R.id.etId);
         etNombreCategoria = findViewById(R.id.etNombreCategoria);
         etDescripcion = findViewById(R.id.etDescripcion);
-        btnIdBuscar = findViewById(R.id.btnIdBuscar);
-        btnEliminar = findViewById(R.id.btnEliminar);
         btnTodosBuscar = findViewById(R.id.btnTodosBuscar);
         btnAgregar = findViewById(R.id.btnAgregar);
-        btnEditar = findViewById(R.id.btnEditar);
         rvCategoria = findViewById(R.id.rvCategoria);
         rvCategoria.setLayoutManager(new GridLayoutManager(this, 1));
 
         retrofit = new AdaptadorRetrofit().getAdapatador();
         api = retrofit.create(CategoriaInterface.class);
-        btnIdBuscar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(etIdBuscar.getText().toString().equals("")) {
-                    Toast.makeText(CategoriaRestActivity.this, "Inserta un ID para buscar", Toast.LENGTH_SHORT).show();
-                } else {
-                    getCategoria(api, etIdBuscar.getText().toString());
-                }
-            }
-        });
-
-        btnEliminar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(etIdBuscar.getText().toString().equals("")) {
-                    Toast.makeText(CategoriaRestActivity.this, "Inserta un ID para eliminar", Toast.LENGTH_SHORT).show();
-                } else {
-                    eliminarCategoria(api, etIdBuscar.getText().toString());
-                }
-            }
-        });
 
         btnTodosBuscar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -88,51 +63,10 @@ public class CategoriaRestActivity extends AppCompatActivity {
             }
         });
 
-        btnEditar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(etId.getText().toString().equals("") || etNombreCategoria.getText().toString().equals("") || etDescripcion.getText().toString().equals("")) {
-                    Toast.makeText(CategoriaRestActivity.this, "Se deben de llenar los campos", Toast.LENGTH_SHORT).show();
-                } else {
-                    editarCategoria(api, etId.getText().toString(), etNombreCategoria.getText().toString(), etDescripcion.getText().toString());
-                }
-            }
-        });
+
     }
-    public void getCategoria(final CategoriaInterface api, String id) {
-        listaCategorias.clear();
-        Call<Categoria> call = api.obtenerCategoria(id);
 
-        call.enqueue(new Callback<Categoria>() {
-            @Override
-            public void onResponse(Call<Categoria> call, Response<Categoria> response) {
-                switch (response.code()) {
-                    case 200:
-                        listaCategorias.add(response.body());
 
-                        etIdBuscar.setText("");
-
-                        adaptadorCategoria = new AdaptadorCategoria(CategoriaRestActivity.this, listaCategorias);
-                        rvCategoria.setAdapter(adaptadorCategoria);
-
-                        break;
-                    case 204:
-                        Toast.makeText(CategoriaRestActivity.this, "No existe ese registro", Toast.LENGTH_SHORT).show();
-
-                        etIdBuscar.setText("");
-
-                        getCategorias(api);
-                        break;
-
-                }
-            }
-
-            @Override
-            public void onFailure(Call<Categoria> call, Throwable t) {
-
-            }
-        });
-    }
 
     public void getCategorias(CategoriaInterface api) {
         listaCategorias.clear();
@@ -156,33 +90,7 @@ public class CategoriaRestActivity extends AppCompatActivity {
         });
     }
 
-    public void eliminarCategoria(final CategoriaInterface api, String id) {
-        listaCategorias.clear();
 
-        Call<Void> call = api.eliminarCategoria(id);
-        call.enqueue(new Callback<Void>() {
-            @Override
-            public void onResponse(Call<Void> call, Response<Void> response) {
-                switch (response.code()) {
-                    case 200:
-                        Toast.makeText(CategoriaRestActivity.this, "Se elimino correctamente", Toast.LENGTH_SHORT).show();
-                        etIdBuscar.setText("");
-                        getCategorias(api);
-                        break;
-                    case 204:
-                        Toast.makeText(CategoriaRestActivity.this, "No se elimino el registro", Toast.LENGTH_SHORT).show();
-                        etIdBuscar.setText("");
-                        break;
-
-                }
-            }
-
-            @Override
-            public void onFailure(Call<Void> call, Throwable t) {
-
-            }
-        });
-    }
 
     public void agregarCategoria(final CategoriaInterface api, String nombreCategoria, String descripcion) {
         listaCategorias.clear();
@@ -215,37 +123,5 @@ public class CategoriaRestActivity extends AppCompatActivity {
         });
     }
 
-    public void editarCategoria(final CategoriaInterface api, String id, String nombreCategoria, String descripcion) {
-        listaCategorias.clear();
-        Categoria categoria = new Categoria();
-        categoria.setId(Integer.parseInt(id));
-        categoria.setNombreCategoria(nombreCategoria);
-        categoria.setDescripcionCategoria(descripcion);
 
-        Call<Void> call = api.editarCategoria(categoria);
-        call.enqueue(new Callback<Void>() {
-            @Override
-            public void onResponse(Call<Void> call, Response<Void> response) {
-                switch (response.code()) {
-                    case 400:
-                        Toast.makeText(CategoriaRestActivity.this, "No se puede editar.", Toast.LENGTH_SHORT).show();
-                        etId.setText("");
-                        etNombreCategoria.setText("");
-                        etDescripcion.setText("");
-                        break;
-                    case 200:
-                        Toast.makeText(CategoriaRestActivity.this, "Se edito correctamente", Toast.LENGTH_SHORT).show();
-                        etId.setText("");
-                        etNombreCategoria.setText("");
-                        etDescripcion.setText("");
-                        getCategorias(api);
-                }
-            }
-
-            @Override
-            public void onFailure(Call<Void> call, Throwable t) {
-
-            }
-        });
-    }
 }
